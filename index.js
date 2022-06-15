43a45a1b653f8485cdf48307982b02e51ea4fb12
Vincent Langat <kimutailangat033@gmail.com>
2022-06-15 10:57:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function addingEventListener() {
     const input = document.getElementById('input');
 
-function clickAlert() {
+function alertClick() {
   alert('I was clicked!');
 }
 
-input.addEventListener('click', clickAlert);
+input.addEventListener('click', alertClick);
 }
